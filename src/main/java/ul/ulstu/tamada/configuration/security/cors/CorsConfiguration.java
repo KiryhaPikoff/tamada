fdbdaee530d82b095281a35dc27642a2e2c37657
kpikov <k.pikov@mediasoft.team>
2020-12-28 21:09:36 +0400

    cors config update

--- src/main/java/ul/ulstu/tamada/configuration/security/cors/CorsConfiguration.java
+++ src/main/java/ul/ulstu/tamada/configuration/security/cors/CorsConfiguration.java
@@ -3,9 +3,11 @@ package ul.ulstu.tamada.configuration.security.cors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
 @Configuration
 public class CorsConfiguration {
 
