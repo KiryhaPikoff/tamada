93f5439dbd39a539a0de1009c08da86b8aee0b89
kpikov <k.pikov@mediasoft.team>
2020-12-29 11:03:48 +0400

    cors allowed all methods

--- src/main/java/ul/ulstu/tamada/configuration/security/cors/CorsConfiguration.java
+++ src/main/java/ul/ulstu/tamada/configuration/security/cors/CorsConfiguration.java
@@ -17,6 +17,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        .allowedMethods("*")
                         .allowedOrigins("*");
             }
         };
